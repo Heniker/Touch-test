64fbd9aacbf22a37b114fcd712f94f73bb818c82
Heniker <art_saif@mail.ru>
2019-08-23 15:13:28 +0300

    initial

--- touchtest.js
+++ touchtest.js
@@ -1,35 +1,186 @@
+const touches = new Map()
+
 document.body.style.touchAction = 'none'
 
-let touches = {}
+window.ontouchstart = onStart
+window.ontouchend = onEnd
+window.ontouchmove = onMove
 
 function onStart(event) {
-  var d = document.createElement('div');
-  d.style.position = "absolute";
-	d.style['border-radius'] = '100%';
-	d.style.width = (~~(event.changedTouches[0].force * 100) - 10) + 'vmin';
-	d.style.height = (~~(event.changedTouches[0].force * 100) - 10) + 'vmin';
-	d.style.backgroundColor = 'blue'
-  d.style.left = event.changedTouches[0].clientX+'px';
-	d.style.top = event.changedTouches[0].clientY+'px';
+	const d = document.createElement('div')
+	const size = ~~(event.changedTouches[0].force * 100) - 10
 
-	touches[event.changedTouches[0].identifier] = d
+	d.size = size
+	d.style.position = 'absolute'
+	d.style.borderRadius = '100%'
+	d.style.width = size + 'vmin'
+	d.style.height = size + 'vmin'
+	d.style.backgroundColor = getRandomColor()
+	d.style.left = `calc(${event.changedTouches[0].clientX}px - ${size/2}vmin)`
+	d.style.top = `calc(${event.changedTouches[0].clientY}px - ${size/2}vmin)`
 
+	touches.set(event.changedTouches[0].identifier, d)
 
 	document.body.appendChild(d)
-}
-window.ontouchstart = onStart
 
+}
 
 function onEnd(event) {
-	document.body.removeChild(touches[event.changedTouches[0].identifier])
+	console.log(event.changedTouches[0].identifier)
+	document.body.removeChild(touches.get(event.changedTouches[0].identifier))
+	touches.delete(event.changedTouches[0].identifier)
 }
-window.ontouchend = onEnd
-
 
 function onMove(event) {
-	Array.from(event.changedTouches).forEach( (e) => {
-		touches[e.identifier].style.left = e.clientX + 'px'
-		touches[e.identifier].style.top = e.clientY + 'px'
+	Array.from(event.changedTouches).forEach((e) => {
+		const elem = touches.get(e.identifier)
+		elem.style.left = `calc(${e.clientX}px - ${elem.size/2}vmin)`
+		elem.style.top = `calc(${e.clientY}px - ${elem.size/2}vmin)`
 	})
 }
-window.ontouchmove = onMove
+
+function getRandomColor() {
+	const colors = [
+		'lightsalmon',
+		'salmon',
+		'darksalmon',
+		'lightcoral',
+		'indianred',
+		'crimson',
+		'firebrick',
+		'red',
+		'darkred',
+		'coral',
+		'tomato',
+		'orangered',
+		'gold',
+		'orange',
+		'darkorange',
+		'lightyellow',
+		'lemonchiffon',
+		'lightgoldenrodyellow',
+		'papayawhip',
+		'moccasin',
+		'peachpuff',
+		'palegoldenrod',
+		'khaki',
+		'darkkhaki',
+		'yellow',
+		'lawngreen',
+		'chartreuse',
+		'limegreen',
+		'lime',
+		'forestgreen',
+		'green',
+		'darkgreen',
+		'greenyellow',
+		'yellowgreen',
+		'springgreen',
+		'mediumspringgreen',
+		'lightgreen',
+		'palegreen',
+		'darkseagreen',
+		'mediumseagreen',
+		'seagreen',
+		'olive',
+		'darkolivegreen',
+		'olivedrab',
+		'lightcyan',
+		'cyan',
+		'aqua',
+		'aquamarine',
+		'mediumaquamarine',
+		'paleturquoise',
+		'turquoise',
+		'mediumturquoise',
+		'darkturquoise',
+		'lightseagreen',
+		'cadetblue',
+		'darkcyan',
+		'teal',
+		'powderblue',
+		'lightblue',
+		'lightskyblue',
+		'skyblue',
+		'deepskyblue',
+		'lightsteelblue',
+		'dodgerblue',
+		'cornflowerblue',
+		'steelblue',
+		'royalblue',
+		'blue',
+		'mediumblue',
+		'darkblue',
+		'navy',
+		'midnightblue',
+		'mediumslateblue',
+		'slateblue',
+		'darkslateblue',
+		'lavender',
+		'thistle',
+		'plum',
+		'violet',
+		'orchid',
+		'fuchsia',
+		'magenta',
+		'mediumorchid',
+		'mediumpurple',
+		'blueviolet',
+		'darkviolet',
+		'darkorchid',
+		'darkmagenta',
+		'purple',
+		'indigo',
+		'pink',
+		'lightpink',
+		'hotpink',
+		'deeppink',
+		'palevioletred',
+		'mediumvioletred',
+		'white',
+		'snow',
+		'honeydew',
+		'mintcream',
+		'azure',
+		'aliceblue',
+		'ghostwhite',
+		'whitesmoke',
+		'seashell',
+		'beige',
+		'oldlace',
+		'floralwhite',
+		'ivory',
+		'antiquewhite',
+		'linen',
+		'lavenderblush',
+		'mistyrose',
+		// 'gainsboro',
+		// 'lightgray',
+		// 'silver',
+		// 'darkgray',
+		// 'gray',
+		// 'dimgray',
+		// 'lightslategray',
+		// 'slategray',
+		// 'darkslategray',
+		// 'black',
+		'cornsilk',
+		'blanchedalmond',
+		'bisque',
+		'navajowhite',
+		'wheat',
+		'burlywood',
+		'tan',
+		'rosybrown',
+		'sandybrown',
+		'goldenrod',
+		'peru',
+		'chocolate',
+		'saddlebrown',
+		'sienna',
+		'brown',
+		'maroon',
+	]
+
+	return colors[Math.floor(Math.random() * colors.length)]
+}
